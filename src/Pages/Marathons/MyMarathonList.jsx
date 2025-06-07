56cb7af57e71d2646da6bf48ca802832713eb17b
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-06-07 20:59:45 +0800

    apply delete operation

--- src/Pages/Marathons/MyMarathonList.jsx
+++ src/Pages/Marathons/MyMarathonList.jsx
@@ -1,11 +1,53 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context/AuthProvider";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const MyMarathonList = () => {
   const { user } = useContext(AuthContext);
   const [myMarathons, setMyMarathons] = useState([]);
+  // handle delete 
+   const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Perform delete action
+        fetch(`http://localhost:3000/marathons/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Marathon has been deleted.",
+                icon: "success",
+              });
 
+              // Remove the deleted group from the state
+              setMyMarathons(myMarathons.filter((marathon) => marathon._id !== id));
+            }
+          })
+          .catch((err) => {
+            console.error("Error deleting group:", err);
+            Swal.fire({
+              title: "Error!",
+              text: "There was an issue deleting the marathon.",
+              icon: "error",
+            });
+          });
+      }
+    });
+  };
+  
+ 
   useEffect(() => {
     if (user?.email) {
       axios
@@ -18,6 +60,7 @@ const MyMarathonList = () => {
         });
     }
   }, [user?.email]);
+  
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4 text-center">Your Created Marathons</h2>
@@ -44,7 +87,7 @@ const MyMarathonList = () => {
                   <td>{marathon.location}</td>
                   <td>
                     <button className="btn btn-xs btn-info mr-2">Update</button>
-                    <button className="btn btn-xs btn-error">Delete</button>
+                    <button onClick={()=>handleDelete(marathon._id)} className="btn btn-xs btn-error">Delete</button>
                   </td>
                 </tr>
               ))}
